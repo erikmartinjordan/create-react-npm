ef89b56f521bb25f9ac4d1b435223227c25ad64e
Erik Martín Jordán <31826994+erikmartinjordan@users.noreply.github.com>
2020-08-12 17:25:47 +0200

    Added cli-select dependency to allow the user to choose the  main component from the cli
    Also added keywords

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fs           = require('fs');
 const { execSync } = require('child_process');
 const readline     = require('readline');
 const chalk        = require('chalk');
+const cliSelect    = require('cli-select');
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Files to create
@@ -46,7 +47,7 @@ const askQuestion = (query) => {
 // authorName: string
 // authorWebsite: string
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const createConfigFiles = (componentName, componentDesc, authorName, authorWebsite) => {
+const createConfigFiles = (mainComponent, componentName, componentDesc, authorName, authorWebsite) => {
     
     let packageConfig = {
         
@@ -69,7 +70,7 @@ const createConfigFiles = (componentName, componentDesc, authorName, authorWebsi
     
     let webpackConfig = {
         
-        "entry": [`./src/${componentName}.js`],
+        "entry": [`./src/${mainComponent}.js`],
         "module": {
             "rules": [
               {
@@ -174,8 +175,13 @@ const buildPackage = () => {
 const checkSrcFolder = () => {
     
     try{
-        if(fs.readdirSync('./src').length > 0)
-            console.log(chalk.green(`✅ /src folder checked!`));
+        if(fs.readdirSync('./src').length > 0){
+            
+            console.log(`\n \n Please select the main file of your component: \n \n`);
+            
+            return fs.readdirSync('./src').filter(file => file.endsWith('js'));
+            
+        }
         else
             throw 'error';
     }
@@ -200,17 +206,21 @@ const main = async () => {
     
     `));
     
-    // Checking if there is src folder
-    checkSrcFolder();
+    // Checking if there is src folder and returns the files in it
+    let files = checkSrcFolder();
+    
+    // User needs to select the main component
+    let mainComponent = (await cliSelect({values: files})).value;
+    console.log(chalk.green(`\n ✅ You have selected ${mainComponent} as the main component. \n`));
     
     // Asking the user some questions
-    let componentName = await askQuestion("What is the name of the main component? Example: awesomeComponent \n");
+    let componentName = await askQuestion("What is the name of the main component? Example: react-awesomeComponent \n");
     let componentDesc = await askQuestion("Describe your component. Example: This is the BeSt coMponEnt EveR \n");
     let authorName    = await askQuestion("What is the name of the author? Example: Elon Musk \n");
     let authorWebsite = await askQuestion("What is the webiste of the author? Example: elonmusk.com \n");
     
     // Creating config files depending on answers
-    createConfigFiles(componentName, componentDesc, authorName, authorWebsite);
+    createConfigFiles(mainComponent, componentName, componentDesc, authorName, authorWebsite);
     
     // Installing dependencies
     await installDependencies(['webpack', 'webpack-dev-server', 'webpack-cli'], 'Webpack');
